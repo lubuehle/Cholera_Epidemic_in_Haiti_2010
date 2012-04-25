976a4956a14117da3c631be7f39f25aa30b70d72
Nicolas Stocker <nstocker@student.ethz.ch>
2012-04-25 15:20:20 +0200

    trying to start using github

--- MSSSM_Cholera/euler.m
+++ MSSSM_Cholera/euler.m
@@ -22,25 +22,46 @@ ds=mu-mu.*s-lambda.*s;
 
 s=s+ds;
 
+if s>=1
+    s=1
+end
+
+if s<=0
+    s=0
+end
+
 
 %Calculations for the variable infectious
 dx=-gamma.*x+lambda.*s-mu.*x;
 
 x=x+dx;
 
+if x>=1
+    x=1
+end
+
+if x<=0
+    x=0
+end
 
 %Calculations for the variable recovered
 
-dr=gamma.*r-mu.*r;
+dr=gamma.*x-mu.*r;
 
 r=r+dr;
+
+
+
+if r<=0
+    r=0
+end
 
 
 
 %Calculations for the variable waterquality
 
 
-dw=epsilon.*(i-w);
+dw=epsilon.*(x-w);
 
 w=w+dw;
 
