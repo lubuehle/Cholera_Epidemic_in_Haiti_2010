a9aece2211a28b907463b9a1fc268c2ea3aac2cd
Nicolas Stocker <nstocker@student.ethz.ch>
2012-05-07 15:38:54 +0200

    improvement of euler function

--- MSSSM_Cholera/euler.m
+++ MSSSM_Cholera/euler.m
@@ -12,32 +12,32 @@ function [s,x, r]=euler(s,x,r, lambda, gamma, mu);
 
 
 
-
-
-
-
-
 %Calculations for the variable susceptible
-ds=mu-lambda.*s-mu.*s;
-
-s=s+ds;
-
+ds=mu-mu.*s-lambda.*s;
 
 %Calculations for the variable infectious
 dx=-gamma.*x+lambda.*s-mu.*x;
 
-x=x+dx;
-
-
 %Calculations for the variable recovered
 
-
 dr=gamma.*x-mu.*r;
+
+%Calculations for the variable waterquality
+
+dw=epsilon.*(x-w);
+
+
+%Updating the concentrations of each variable
+
+s=s+ds;
+
+x=x+dx;
 
 r=r+dr;
 
 %r=1-s-x;
 
+w=w+dw;
 
 
 
